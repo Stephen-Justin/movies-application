8e2d3b89de6f013f15549e13af1c1d4a27dfcc37
Justin Wayne Wilson <wilsonjustinwayne@gmail.com>
2018-07-19 12:55:52 -0500

    AddMovies Completed

--- src/index.js
+++ src/index.js
@@ -1,35 +1,53 @@
 /**
  * es6 modules and imports
  */
-import sayHello from './hello';
-sayHello('World');
 import $ from 'jquery';
-
-
 import {getMovies, addMovies} from './api';
+import sayHello from './hello';
 
+sayHello('World');
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    // console.log(`id#${id} - ${title} - rating: ${rating}`);
-    $('#movie-info').append(`<li> id#${id} - ${title} - rating: ${rating} </li>`);
-
-  });
-    loader.removeClass("visible");
-    loader.addClass("invisible");
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.');
-  console.log(error);
-});
 const loader = $('#loading');
-$(document).ready(() => {
-  loader.addClass("visible")
+const movieInfo = $('#movie-info');
+let movieCount = 0;
+
+function refreshMovies() {
+    movieInfo.html(``);
+    getMovies().then((movies) => {
+        movies.forEach(({title, rating, id}) => {
+            // console.log(`id#${id} - ${title} - rating: ${rating}`);
+            movieCount = parseInt(id);
+            console.log(id);
+            movieInfo.append(`<li> id#${id} - ${title} - rating: ${rating} </li>`);
+
+        });
+        loader.removeClass("visible");
+        loader.addClass("invisible");
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.');
+        console.log(error);
+    });
+}
+
+
+function loadUp() {
+    loader.removeClass('invisible');
+    loader.addClass("visible")
+}
+
+$('#addMovie').click((e) => {
+    e.preventDefault();
+    let data = {
+        title: $('#movieTitle').val(),
+        rating: $('#movieRating').val()
+    };
+    loadUp();
+    addMovies(data);
+    refreshMovies();
 });
 
-addMovies();
 
-
-
-
-
+$(document).ready(() => {
+    loadUp();
+    refreshMovies();
+});
